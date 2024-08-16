4717224928d66f066d3d4a8d29587dcdff211b30
Gene Flores <goflores@usc.edu>
2024-08-16 10:56:14 -0700

    Teaching Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import angesPic from "./_imgs/anges.jpg"
 export default function Home() {
   return (
     <main className="relative" >
-      <section className="mx-auto relative h-screen " id="about">
+      <section className="mx-auto relative min-h-screen " id="about">
         <div className="w-full flex flex-col gap-y-4 justify-center py-16 z-10 relative">
           <h1 className="text-6xl font-bold text-center">Anges Vu</h1>
           <h2 className="text-xl font-bold text-center">PhD Student</h2>
@@ -12,7 +12,7 @@ export default function Home() {
         <div id="aboutme"className="p-16 flex md:flex-row-reverse flex-col justify-center gap-x-12 z-10 gap-y-8 relative max-w-7xl mx-auto bg-white">
           <div className="w-full relative z-10 flex flex-col"> 
               <div className="p-4 bg-tiffany">
-                <Image priority className="" src={angesPic} alt="Anges Image" sizes="(max-width: 768px) 40vw, (max-width: 1200px) 25vw, 10vw"></Image>
+                <Image priority className="" src={angesPic} alt="Anges Image" sizes="(max-width: 768px) 30vw, (max-width: 1200px) 25vw, 10vw"></Image>
               <div/>
               </div>
             </div>
@@ -31,9 +31,24 @@ export default function Home() {
         {/* <div className="absolute spacer hero-bg"></div> */}
       </section>
       
-      <section id="teaching" className="h-screen bg-tiffany">
+      <section id="teaching" className="min-h-screen bg-tiffany py-16">
+        <h1 className="text-6xl font-bold text-center">Teaching</h1>
+          <div className="flex flex-col gap-y-4 justify-center py-16 z-10 relative items-center md:flex-row md:px-16">
+              <div className="p-4 teaching-card w-full max-w-96">
+                <div className="flex flex-col gap-y-4 text-center bg-white shadow-lg p-8">
+                  <i className="fa-solid fa-school text-tiffany text-8xl"></i>
+                  <h2 className="text-lg font-bold">Sound Patterns in Human Language</h2>
+                  <p className="text-sm">[Fall 2024] Department of Linguistics, Northwestern University</p>
+                  <p className="text-sm">Instructor: Jennifer Cole</p>
+                </div>
+              </div>
+            </div>
+
+
       </section>
-      <section id="research"className="h-screen bg-transparent">
+      <section id="research"className="min-h-screen bg-transparent py-16">
+        <h1 className="text-6xl font-bold text-center">Research</h1>
+
       </section>
     </main>
   );
